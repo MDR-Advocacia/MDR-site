38cec1ba198801a80c2a44979df31d8d4ace906e
joseantonioneto <antonio.souzneto@gmail.com>
2025-07-30 10:16:07 -0300

    Atualizando o index

--- index.php
+++ index.php
@@ -2,82 +2,22 @@
 
 <section id="hero" class="hero section dark-background">
 
-  <img src="assets/img/hero-bg.jpg" alt="" data-aos="fade-in">
+  <img src="assets/img/hero2-bg.jpg" alt="" data-aos="fade-in">
 
   <div class="container">
     <div class="row">
       <div class="col-xl-4">
         <h2 data-aos="fade-up">Integridade, eficiência e tecnologia, a serviço da advocacia sustentável e por excelência.</h2>
         <br><br>
-        <div class="d-flex" data-aos="fade-up" data-aos-delay="200">
-          <a href="about.php" class="btn-get-started">Quem somos?</a>
+        <div class="watch-video-container" data-aos="fade-up" data-aos-delay="200">
+          <a href="https://www.youtube.com/watch?v=gF2ZvY7DpKs" class="glightbox btn-watch-video d-flex align-items-center"><i class="bi bi-play-circle-fill"></i><span>Quem somos?</span></a>
         </div>
       </div>
     </div>
   </div>
 
 </section>
-<section id="why-us" class="why-us section">
-</section>
-<section id="services" class="services section">
 
-  <div class="container section-title" data-aos="fade-up">
-    <h2>Nossa atuação</h2>
-  </div>
-  <div class="container">
 
-    <div class="row gy-4">
-
-      <div class="col-lg-4 col-md-6 service-item d-flex" data-aos="fade-up" data-aos-delay="100">
-        <div class="icon flex-shrink-0"><i class="bi bi-briefcase" style="color: #f57813;"></i></div>
-        <div>
-          <h4 class="title">Direito Bancário</h4>
-          <p class="description">Com uma equipe especializada em Direito Bancário, nós entendemos a importância dessa matéria na economia e estamos preparados para lidar com os desafios legais e regulatórios que envolvem essa área.</p>
-          <a href="service1.php" class="readmore stretched-link"><span>Saiba mais</span><i class="bi bi-arrow-right"></i></a>
-        </div>
-      </div>
-      <div class="col-lg-4 col-md-6 service-item d-flex" data-aos="fade-up" data-aos-delay="200">
-        <div class="icon flex-shrink-0"><i class="bi bi-card-checklist" style="color: #15a04a;"></i></div>
-        <div>
-          <h4 class="title">Direito Regulatório</h4>
-          <p class="description">Nosso escritório conta com uma equipe especializada em Direito Regulatório e focada em todas as complexidades que advém dessa esfera. Sempre atualizados, nossos serviços são voltados a compreender todas as leis e regulamentos que governam indústrias e setores específicos, entre outros</p>
-          <a href="service2.php" class="readmore stretched-link"><span>Saiba mais</span><i class="bi bi-arrow-right"></i></a>
-        </div>
-      </div>
-      <div class="col-lg-4 col-md-6 service-item d-flex" data-aos="fade-up" data-aos-delay="300">
-        <div class="icon flex-shrink-0"><i class="bi bi-bar-chart" style="color: #d90769;"></i></div>
-        <div>
-          <h4 class="title">Direito Fiscal e Tributário</h4>
-          <p class="description">Nossa equipe oferece uma gama abrangente de serviços em Direito Fiscal e Tributário. Somos focados em auxiliar empresas e indivíduos a entender suas obrigações fiscais, desenvolvendo estratégias de planejamento tributário personalizadas a fim de prevenir prejuízos e reduzir a carga tributária.</p>
-          <a href="service3.php" class="readmore stretched-link"><span>Saiba mais</span><i class="bi bi-arrow-right"></i></a>
-        </div>
-      </div>
-      <div class="col-lg-4 col-md-6 service-item d-flex" data-aos="fade-up" data-aos-delay="400">
-        <div class="icon flex-shrink-0"><i class="bi bi-binoculars" style="color: #15bfbc;"></i></div>
-        <div>
-          <h4 class="title">Comércio Internacional</h4>
-          <p class="description">Somos um escritório de advocacia especializado em assuntos relacionados ao Comércio Internacional, oferecendo suporte legal abrangente para empresas que atuam no mercado global.</p>
-          <a href="service4.php" class="readmore stretched-link"><span>Saiba mais</span><i class="bi bi-arrow-right"></i></a>
-        </div>
-      </div>
-      <div class="col-lg-4 col-md-6 service-item d-flex" data-aos="fade-up" data-aos-delay="500">
-        <div class="icon flex-shrink-0"><i class="bi bi-brightness-high" style="color: #f5cf13;"></i></div>
-        <div>
-          <h4 class="title">Planejamento Sucessório</h4>
-          <p class="description">No nosso escritório, entendemos a importância de planejar o futuro e proteger o patrimônio e o bem-estar da sua família. Nosso foco é oferecer serviços especializados em planejamento sucessório, com uma abordagem moderna e ética, para garantir uma transição tranquila e segura para as gerações futuras.</p>
-          <a href="service5.php" class="readmore stretched-link"><span>Saiba mais</span><i class="bi bi-arrow-right"></i></a>
-        </div>
-      </div>
-      <div class="col-lg-4 col-md-6 service-item d-flex" data-aos="fade-up" data-aos-delay="600">
-        <div class="icon flex-shrink-0"><i class="bi bi-calendar4-week" style="color: #1335f5;"></i></div>
-        <div>
-          <h4 class="title">Seguros</h4>
-          <p class="description">Nós reconhecemos a importância de proteger o patrimônio e garantir a segurança financeira dos nossos clientes. Também somos especialistas em oferecer serviços personalizados em seguros, com uma abordagem moderna para proporcionar tranquilidade e proteção para o presente e para o futuro.</p>
-          <a href="service6.php" class="readmore stretched-link"><span>Saiba mais</span><i class="bi bi-arrow-right"></i></a>
-        </div>
-      </div>
-    </div>
-
-  </div>
 
 </section><?php include 'footer.php'; ?>
