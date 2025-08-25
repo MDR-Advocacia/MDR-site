c64af6029b64a1f4e0fb403e161215decf94570e
Murilo Silva <murilo.silva.136@ufrn.edu.br>
2025-08-25 10:16:14 -0300

    atualização dos depoimentos

--- recuperacao-credito.php
+++ recuperacao-credito.php
@@ -313,6 +313,111 @@
             });
         });
     </script>
+        </section><section id="testimonials" class="testimonials section">
+
+      <div class="container section-title" data-aos="fade-up">
+        <h2>Depoimentos</h2>
+        <p>Seu Feedback nos inspira!</p>
+      </div><div class="container" data-aos="fade-up" data-aos-delay="100">
+
+        <div class="swiper init-swiper">
+          <script type="application/json" class="swiper-config">
+            {
+              "loop": true,
+              "speed": 600,
+              "autoplay": {
+                "delay": 5000
+              },
+              "slidesPerView": "auto",
+              "pagination": {
+                "el": ".swiper-pagination",
+                "type": "bullets",
+                "clickable": true
+              },
+              "breakpoints": {
+                "320": {
+                  "slidesPerView": 1,
+                  "spaceBetween": 40
+                },
+                "1200": {
+                  "slidesPerView": 3,
+                  "spaceBetween": 1
+                }
+              }
+            }
+          </script>
+          <div class="swiper-wrapper">
+
+            <div class="swiper-slide">
+              <div class="testimonial-item">
+                <div class="stars">
+                  <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
+                </div>
+                <p>
+                  Escritório de excelência, com profissionais extremamente qualificados.
+                </p>
+                <div class="profile mt-auto">
+                  <img src="assets/img/testimonials/testimonials-1.png" class="testimonial-img" alt="">
+                  <h3>Gabriel Abner</h3>
+                </div>
+              </div>
+            </div><div class="swiper-slide">
+              <div class="testimonial-item">
+                <div class="stars">
+                  <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
+                </div>
+                <p>
+                  O melhor e mais conceituado serviço de advocacia que conheço , Ótimos profissionais e um atendimento que não deixa a desejar em nada .
+                </p>
+                <div class="profile mt-auto">
+                  <img src="assets/img/testimonials/testimonials-2.png" class="testimonial-img" alt="">
+                  <h3>Plinio Neto</h3>
+                </div>
+              </div>
+            </div><div class="swiper-slide">
+              <div class="testimonial-item">
+                <div class="stars">
+                  <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
+                </div>
+                <p>
+                  Atendimento com ótimos profissionais capacitados, ambiente aconchegante. Empresa organizada e ótima localização!
+                </p>
+                <div class="profile mt-auto">
+                  <img src="assets/img/testimonials/testimonials-3.png" class="testimonial-img" alt="">
+                  <h3>Ana Virgínia</h3>
+                </div>
+              </div>
+            </div><div class="swiper-slide">
+              <div class="testimonial-item">
+                <div class="stars">
+                  <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
+                </div>
+                <p>
+                  Um local ótimo, tanto para atendimento quanto para trabalho. Incrível!
+                </p>
+                <div class="profile mt-auto">
+                  <img src="assets/img/testimonials/testimonials-4.png" class="testimonial-img" alt="">
+                  <h3>Gabriela Rodrigues</h3>
+                </div>
+              </div>
+            </div><div class="swiper-slide">
+              <div class="testimonial-item">
+                <div class="stars">
+                  <i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i>
+                </div>
+                <p>
+                  Atendimento maravilhoso,  desde o atendimento telefônico até o final. Todos atenciosos e prestativos.  Local de fácil localização,  com bom local para estacionar. Ambiente agradável de estar. Nota dez. Faz a gente sentir-se importante, acolhida e ouvida. Um exemplo a ser seguido. Parabéns!
+                </p>
+                <div class="profile mt-auto">
+                  <img src="assets/img/testimonials/testimonials-5.png" class="testimonial-img" alt="">
+                  <h3>Neuza Mendonça</h3>
+                </div>
+              </div>
+            </div></div>
+          <div class="swiper-pagination"></div>
+        </div>
+
+      </div>
 
     <?php include 'footer.php'; ?>
 
