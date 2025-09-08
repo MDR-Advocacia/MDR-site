802c6363a237bc74c670a506686d4e530d0f279c
murilosilvaof <109997059+murilosilvaof@users.noreply.github.com>
2025-09-08 11:33:06 -0300

    atualização de pagina final ,ultimos ajustes

--- recuperacao-credito.php
+++ recuperacao-credito.php
@@ -204,7 +204,7 @@
             <div class="professional-card-details">
                 <h4>Diego Alves do Nascimento</h4><hr>
                 <p class="title">Coordenador</p>
-                <p class="sector">Gestão de Risco</p>
+                <p class="sector"></p>
                 <ul class="contact-list">
                     <li><span><svg class="icon-svg" viewBox="0 0 24 24"><path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/></svg></span> Natal / RN</li>
                     <li><span><svg class="icon-svg" viewBox="0 0 24 24"><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/></svg></span> Diego.nascimento@mdradvocacia.com</li>
@@ -356,4 +356,5 @@
 </a>
 <?php 
   include 'footer.php'; 
+
 ?>
