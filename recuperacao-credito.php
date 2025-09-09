b203f45be54423d88341d00f0121018d90fb5adb
murilosilvaof <109997059+murilosilvaof@users.noreply.github.com>
2025-09-09 10:59:17 -0300

    Adicionado Melissa

--- recuperacao-credito.php
+++ recuperacao-credito.php
@@ -274,6 +274,26 @@
                 </ul>
             </div>
         </div>
+                <div class="professional-card animar-ao-rolar">
+            <div class="profile-pic-container">
+                <img src="assets/img/supervisores/placeholder.jpg" alt="Melissa" class="profile-image">
+                <div class="social-icons">
+                    <a href="#" target="_blank" aria-label="LinkedIn de Melissa"><svg viewBox="0 0 24 24" style="width: 24px; height: 24px; fill: #555;"><path d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14m-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.32 1.3v-1.11h-3.06v8.37h3.06v-4.93c0-.83.06-1.66.57-2.12.51-.46 1.28-.48 1.76 0 .5.47.48 1.3.48 2.12v4.93h3.06zM6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69.75 1.68 1.68 0 0 0 0 1.88 1.68 1.68 0 0 0 1.69.74zm-1.44 9.94h3.06v-8.37H5.44v8.37z"/></svg></a>
+                    <a href="#" target="_blank" aria-label="Instagram de Melissa"><svg viewBox="0 0 24 24" style="width: 24px; height: 24px; fill: #555;"><path d="M7.8,2H16.2C19.4,2 22,4.6 22,7.8V16.2A5.8,5.8 0 0,1 16.2,22H7.8C4.6,22 2,19.4 2,16.2V7.8A5.8,5.8 0 0,1 7.8,2M7.6,4A3.6,3.6 0 0,0 4,7.6V16.4C4,18.39 5.61,20 7.6,20H16.4A3.6,3.6 0 0,0 20,16.4V7.6C20,5.61 18.39,4 16.4,4H7.6M17.25,5.5A1.25,1.25 0 0,1 18.5,6.75A1.25,1.25 0 0,1 17.25,8A1.25,1.25 0 0,1 16,6.75A1.25,1.25 0 0,1 17.25,5.5M12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9Z"/></svg></a>
+                    <a href="#" target="_blank" aria-label="Whatsapp de Melissa"><svg viewBox="0 0 24 24" style="width: 24px; height: 24px; fill: #555;"><path d="M16.75,13.96C17.0,14.21 17.2,14.5 17.2,14.86C17.2,15.22 17.05,15.57 16.8,15.82C16.55,16.07 16.21,16.22 15.85,16.22C15.31,16.22 13.8,15.77 12.04,14.22C10,12.41 8.7,10.27 8.7,9.81C8.7,9.35 8.85,9 9.1,8.75C9.35,8.5 9.6,8.35 9.96,8.35C10.32,8.35 10.6,8.5 10.85,8.75C11.1,9 11.3,9.29 11.3,9.65C11.3,10.01 11.15,10.36 10.9,10.61L10.5,11.01C10.3,11.21 10.11,11.4 10.35,11.65C10.59,11.9 11.23,12.54 11.96,13.27C12.91,14.22 13.54,14.58 13.9,14.58C14.26,14.58 14.5,14.38 14.7,14.18L15.1,13.78C15.35,13.53 15.7,13.38 16.06,13.38C16.42,13.38 16.77,13.53 17.02,13.78L17.47,14.23C17.22,14.48 16.97,14.73 16.75,13.96M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4Z"/></svg></a>
+                </div>
+            </div>
+            <div class="professional-card-details">
+                <h4>Melissa Cristine de Oliveira e Santos</h4><hr>
+                <p class="title">Supervisora</p>
+                <p class="sector">OAB/RN 22.188 </p>
+                <ul class="contact-list">
+                    <li><span><svg class="icon-svg" viewBox="0 0 24 24"><path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/></svg></span> Natal / RN</li>
+                    <li><span><svg class="icon-svg" viewBox="0 0 24 24"><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/></svg></span>melissa.santos@mdradvocacia.com</li>
+                    <li><span><svg class="icon-svg" viewBox="0 0 24 24"><path d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z"/></svg></span> (84) 98139-1998</li>
+                </ul>
+            </div>
+        </div>
 
         <div class="professional-card animar-ao-rolar">
             <div class="profile-pic-container">
@@ -286,7 +306,7 @@
             </div>
             <div class="professional-card-details">
                 <h4>Sthéfanie de Melo Medeiros Queiroz</h4><hr>
-                <p class="title">Advogada Supervisora</p>
+                <p class="title">Supervisora</p>
                 <p class="sector">OAB/RN 20.347</p>
                 <ul class="contact-list">
                     <li><span><svg class="icon-svg" viewBox="0 0 24 24"><path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/></svg></span> Natal / RN</li>
